4cd515d021738ae6ba278d64f26293565befad8e
Adolfo <ja.gutierrez.gaytan@ugto.mx>
2021-12-07 22:22:29 -0600

    Correcion de tabla mostrar bitacora

--- src/index.js
+++ src/index.js
@@ -1,56 +1,57 @@
-const express = require('express');
-const path = require('path');
-const multer = require('multer');
-const engine = require('ejs-mate');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const morgan = require('morgan');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const engine = require("ejs-mate");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const morgan = require("morgan");
+const { v4: uuidv4 } = require("uuid");
 
 // initializations
 const app = express();
-require('./database');
-require('./passport/local-auth');
+require("./database");
+require("./passport/local-auth");
 
 // settings
 app.listen(3000);
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'public/img/up'),
-  filename: (req, file, cb, filename) => {
+  destination: path.join(__dirname, "public/img/up"),
+    filename: (req, file, cb, filename) => {
       console.log(file);
       cb(null, uuidv4() + path.extname(file.originalname));
-  }
-}) 
-app.use(multer({storage: storage}).single('image'));
-app.use(express.static(__dirname + '/public'));
-app.engine('ejs',engine);
-app.set('view engine', 'ejs');
+    },
+});
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(multer({ storage: storage }).single("image"));
+app.use(express.static(__dirname + "/public"));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
+app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
     secret: "No temo al dolor por que ya nacio a mi lado",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-    app.locals.signinMessage = req.flash('signinMessage');
-    app.locals.signupMessage = req.flash('signupMessage');
-    app.locals.user = req.user;
-    console.log(app.locals)
-    next();
-  });;
+  app.locals.signinMessage = req.flash("signinMessage");
+  app.locals.signupMessage = req.flash("signupMessage");
+  app.locals.user = req.user;
+  console.log(app.locals);
+  next();
+});
 
-app.use('/',require('./routes/index'));
+app.use("/", require("./routes/index"));
 
-
-
-console.log('servidor en el ',3000);
+console.log("servidor en el ", 3000);
